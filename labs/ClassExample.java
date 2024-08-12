aac6ed98b8e489948b4b80361a0d6ffd4e9eed41
kat-ash59 <cliffykat@gmail.com>
2024-08-12 16:54:15 -0600

    renamed file to match class name in example

---
+++ labs/ClassExample.java
@@ -0,0 +1,11 @@
+//
+// this is the java program for objective1 for lab1
+//
+public class ClassExample
+{
+	public static void main (String[] args)
+	{
+		// this is where we will write our code
+		System.out.println("\n\n\tThese words will be printed.\n\n");
+	}
+}
