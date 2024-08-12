17cb7199eaacba8b0cb911a11f1f6d7c99cf064d
kat-ash59 <cliffykat@gmail.com>
2024-08-12 17:31:19 -0600

    completed objective1-sysout

---
+++ labs/SysoutExample.java
@@ -0,0 +1,17 @@
+//
+// this is the java program for objective1 for lab3
+//
+public class SysoutExample
+{
+	public static void main (String[] args)
+	{
+		// this is where I have added my code
+		// adding blank lines before
+		System.out.println("\n\n");
+		System.out.println("\tThese words will be printed.");
+		System.out.println("\tAnd so will these words.");
+		System.out.println("\tAnd finally these words.");
+		// adding blank lines after
+		System.out.println("\n\n");
+	}
+}
