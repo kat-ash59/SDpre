cf8ae474f02f73b8c782e82868fd47999d3e6647
kat-ash59 <cliffykat@gmail.com>
2024-08-12 17:53:56 -0600

    adding String Example into repository

---
+++ labs/StringExample.java
@@ -0,0 +1,27 @@
+//
+// Objective 2 programs
+// using variables
+//
+
+public class StringExample
+{
+	// really not sure why these integers were added in the comments for the objective, but here they are
+	// not doing anything with them at the moment
+	public static int first_number = 1;
+	public static int second_number = 2;
+	public static int sum = first_number + second_number;
+
+	public static void main(String[] args)
+	{
+		
+
+		String withoutQuotes = "This is a sentence without quotes.";
+		String withQuotes = "This is a sentence with \"quotes\"";
+		System.out.println("\n\n");
+		System.out.println(withoutQuotes);
+		System.out.println("\n\n");
+		System.out.println(withQuotes);
+		System.out.println("\n\n");
+	}
+}
+		
