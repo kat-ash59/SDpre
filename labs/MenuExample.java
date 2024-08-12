d35db8aa14d5012145c8da3a3e1af3a4824ba0dd
kat-ash59 <cliffykat@gmail.com>
2024-08-12 17:25:10 -0600

    complete objective1lab3

---
+++ labs/MenuExample.java
@@ -0,0 +1,17 @@
+//
+// this is the java program for objective1 for lab3
+//
+public class MenuExample
+{
+	public static void main (String[] args)
+	{
+		// this is where we will write our code
+		System.out.println("\n\n");
+		System.out.println("\tWelcome to my Bank Application!");
+		System.out.println("\t-------------------------------");
+		System.out.println("\t1) Check Balance");
+		System.out.println("\t2) Withdraw");
+		System.out.println("\t3) Deposit");
+		System.out.println("\t-------------------------------\n\n");
+	}
+}
