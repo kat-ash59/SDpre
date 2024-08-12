e9011bdef92dc2e111399875e0d21a90e173dbe7
kat-ash59 <cliffykat@gmail.com>
2024-08-12 16:43:52 -0600

    Yeahhh Hello World Java Style

---
+++ labs/HelloWorld.java
@@ -0,0 +1,12 @@
+// 
+// hello world Java style
+//
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		System.out.println("\n\n");
+		System.out.println("\tHello, World!");
+		System.out.println("\n\n");
+	}
+}
