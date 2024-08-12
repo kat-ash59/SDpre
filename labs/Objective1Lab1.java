9d5068c755788d0f12d2dcd9147b4248fb6006a6
kat-ash59 <cliffykat@gmail.com>
2024-08-12 17:07:31 -0600

    completed objective1lab1

---
+++ labs/Objective1Lab1.java
@@ -0,0 +1,11 @@
+//
+// this is the java program for objective1 for lab1
+//
+public class Objective1Lab1
+{
+	public static void main (String[] args)
+	{
+		// this is where we will write our code
+		System.out.println("\n\n\tHello Skill Distillery!\n\n");
+	}
+}
