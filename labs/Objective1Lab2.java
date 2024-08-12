2374f65e07d3b84b8c5683aee50304faa29bbf0f
kat-ash59 <cliffykat@gmail.com>
2024-08-12 17:19:26 -0600

    completed Objective1Lab2

---
+++ labs/Objective1Lab2.java
@@ -0,0 +1,17 @@
+//
+// this is the java program for objective1 for lab2
+//
+public class Objective1Lab2
+{
+	public static void main (String[] args)
+	{
+		// this is where we will write our code
+		System.out.println("\n\n");
+		System.out.println("\t-----------------------------");
+		System.out.println("\t|       Grocery List        |");
+		System.out.println("\t| 1. Milk                   |");
+		System.out.println("\t| 2. Eggs                   |");
+		System.out.println("\t| 3. Bread                  |");
+		System.out.println("\t-----------------------------\n\n");
+	}
+}
