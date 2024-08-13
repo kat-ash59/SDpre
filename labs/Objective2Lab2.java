914456a35420cef030cff4cf8b7cdf930d124782
kat-ash59 <cliffykat@gmail.com>
2024-08-12 18:03:43 -0600

    completed Objective2 Lab2

---
+++ labs/Objective2Lab2.java
@@ -0,0 +1,19 @@
+//
+// Objective 2 programs
+// using variables
+//
+
+public class Objective2Lab2
+{
+		public static void main(String[] args)
+	{
+		String name = "Kat Ash";
+		int age = 65;
+		String state = "Colorado";
+		
+		System.out.println("\n\n");
+		System.out.println("My Name is: " + name + "\tMy age is: " + age + "\tThe state I live in is: " + state);
+		System.out.println("\n\n");
+	}
+}
+		
