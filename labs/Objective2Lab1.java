f03575154f2e7d54a0e511a1db6dc07e381c47ff
kat-ash59 <cliffykat@gmail.com>
2024-08-12 18:04:59 -0600

    complete Objective2 Lab1

---
+++ labs/Objective2Lab1.java
@@ -0,0 +1,18 @@
+//
+// Objective 2 programs
+// using variables
+//
+
+public class Objective2Lab1
+{
+		public static void main(String[] args)
+	{
+		
+
+		String hello = "Hello World!";
+		System.out.println("\n\n");
+		System.out.println(hello);
+		System.out.println("\n\n");
+	}
+}
+		
