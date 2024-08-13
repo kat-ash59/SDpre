f34ae000089e3497108c811b5b27cc5efcab693a
kat-ash59 <cliffykat@gmail.com>
2024-08-12 18:10:52 -0600

    completed Objective 2 Lab 3

---
+++ labs/Objective2Lab3.java
@@ -0,0 +1,22 @@
+//
+// Objective 2 programs
+// using variables
+//
+
+public class Objective2Lab3
+{
+		public static void main(String[] args)
+	{
+		// integer for the year i was born in
+		int year = 1959;
+		// string for the month i was born in
+		String month = "July";
+		// Is today a weekday or is it the weekend
+		String dayofweek = "Today is a weekday";
+		
+		System.out.println("\n\n");
+		System.out.println("The year I was born in is: " + year + "\nThe month I was born in is: " + month + "\nIs today a weekday or is it the weekend: " + dayofweek);
+		System.out.println("\n\n");
+	}
+}
+		
