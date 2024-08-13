5b1a0b7b8753d20fa4d749c3b8da8c2548c684a2
kat-ash59 <cliffykat@gmail.com>
2024-08-12 18:17:50 -0600

    changed Objective 2 Lab 3 to have a boolean rather than a string for variable

--- labs/Objective2Lab3.java
+++ labs/Objective2Lab3.java
@@ -12,10 +12,12 @@ public class Objective2Lab3
 		// string for the month i was born in
 		String month = "July";
 		// Is today a weekday or is it the weekend
-		String dayofweek = "Today is a weekday";
+		// since I am writing this on a Monday the value will be true since it is a weekday
+		Boolean dayofweek = true;
 		
 		System.out.println("\n\n");
-		System.out.println("The year I was born in is: " + year + "\nThe month I was born in is: " + month + "\nIs today a weekday or is it the weekend: " + dayofweek);
+		// putting them all on the same line but adding newline and tab before each variable
+		System.out.println("\tThe year I was born in is: " + year + "\n\tThe month I was born in is: " + month + "\n\tIs today a weekday (will be true) or is it the weekend (will be false): " + dayofweek);
 		System.out.println("\n\n");
 	}
 }
