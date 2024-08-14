f06ac79b43551a5a1a8a855e20dff39279b82d39
kat-ash59 <cliffykat@gmail.com>
2024-08-14 12:06:41 -0600

    completed Objective 7 Lab 1

---
+++ labs/Objective7Lab1.java
@@ -0,0 +1,38 @@
+//
+// Objective 7 programs
+// using do while loops
+//
+// Create a file called Objective7Lab1.java in the SDPre/Labs directory.
+// Using the starter code below, add code to the commented areas that would:
+// Print the values 10 to 1.
+// Print the value of counter with each iteration.
+
+
+// need to be able to use import to get input variables
+import java.time.*;
+import java.util.*;
+
+public class Objective7Lab1
+{
+	public static void main(String[] args)
+	{
+		// now declaring an integer with the value of the number 10
+		int counter = 10;
+		
+		// printing out the counter from 10 to 1
+		// using a do while loop
+	
+		
+		while (counter > 0)
+		{
+			System.out.println("\n\tThe value of counter = " + counter + "\n");
+			counter = counter - 1;
+		}
+		
+	
+	}
+	
+	
+	
+}
+		
